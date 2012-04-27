d53b55e2c00b46b181a1574d2d12a3da4b4a2d87
Tommy MacWilliam <tmacwilliam@cs.harvard.edu>
2012-04-27 17:26:33 -0400

    added database config to installer
    - also drafted first attempt at a landing page

---
+++ client/app/Config/Schema/schema.php
@@ -0,0 +1,35 @@
+<?php 
+class AppSchema extends CakeSchema {
+
+	public function before($event = array()) {
+		return true;
+	}
+
+	public function after($event = array()) {
+	}
+
+	public $documents = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'filename' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'md5' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 32, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'sha1' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 40, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'path' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+	public $hosts = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'document_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'url' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+	public $users = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'email' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'password' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 40, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+}
--- server/app/View/Pages/home.ctp
+++ server/app/View/Pages/home.ctp
@@ -1,9 +1,22 @@
 <div class="container-fluid">
     <h1 id="main-header">Verify your documents' integrity.</h1>
-    <div id="home-features">
+    <div id="home-features" class="row-fluid">
+        <div class="span6">
+            <h2>Upload your documents for others to host.</h2>
+            <p class="feature-description">
+                Ensure your content is kept safe. Uploading your document means any number of people can host it and keep it safe for you. With an entire network of providers, there's no single point of failure.
+            </p>
+        </div>
 
-        <div style="text-align: center">
-            <a class="btn btn-primary btn-large" href="<?= $this->webroot ?>hosts/add">Sign up now.</a>
+        <div class="span6" style="margin-bottom: 20px">
+            <h2>The integrity of your documents is automatically verified.</h2>
+            <p class="feature-description">
+                Make sure your content isn't changed without you knowing. Your content will be verified against the network of individuals hosting it, and if anyone's (including yours) copy is changed, they'll be notified.
+            </p>
+        </div>
+
+        <div class="row-fluid" style="clear: both">
+            <a class="btn btn-primary btn-large" href="<?= $this->webroot ?>hosts/add">Sign up for free.</a>
         </div>
     </div>
 </div>
